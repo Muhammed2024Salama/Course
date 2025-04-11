75a9cd79bf691af371da17b629f32644aea4b3f8
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-04-11 19:18:32 +0200

    Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,5 @@ Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+Route::resource('/blog',BlogController::class);
